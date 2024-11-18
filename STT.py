c39a81ba5a83980c169fb6fc042d6d37c24e7992
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2024-11-18 02:51:27 -0800

    Basic Speech to Text into terminal

--- STT.py
+++ STT.py
@@ -4,18 +4,48 @@ import queue
 import sys
 import json
 
-model = Model(r'C:\Users\vahl_\Projects\STT\model')
-recognizer = KaldiRecognizer(model, 16000)
+print("Display input/output devices")
+print(sd.query_devices())
 
-#recognize the microphone
-cap = pyaudio.PyAudio()
-stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-stream.start_stream()
+# get the samplerate - this is needed by the Kaldi recognizer
+device_info = sd.query_devices(sd.default.device[0], 'input')
+samplerate = int(device_info['default_samplerate'])
 
-while True:
-    data = stream.read(4096)
-    if len(data) == 0:
-        break
+# display the default input device
+print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info))
 
-    if recognizer.AcceptWaveform(data):
-        print(recognizer.Result)
+# setup queue and callback function
+q = queue.Queue()
+
+def recordCallback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+
+# build the model and recognizer objects.
+print("===> Build the model and recognizer objects.  This will take a few minutes.")
+model = Model(r"C:\Users\vahl_\Projects\STT\model")
+recognizer = KaldiRecognizer(model, samplerate)
+recognizer.SetWords(False)
+
+print("===> Begin recording. Press Ctrl+C to stop the recording ")
+try:
+    with sd.RawInputStream(dtype='int16',
+                           channels=1,
+
+                           callback=recordCallback):
+        while True:
+            data = q.get()        
+            if recognizer.AcceptWaveform(data):
+                recognizerResult = recognizer.Result()
+                # convert the recognizerResult string into a dictionary  
+                resultDict = json.loads(recognizerResult)
+                if not resultDict.get("text", "") == "":
+                    print(recognizerResult)
+                else:
+                    print("no input sound")
+
+except KeyboardInterrupt:
+    print('===> Finished Recording')
+except Exception as e:
+    print(str(e))
